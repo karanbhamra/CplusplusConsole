758789bfde9d9d658ca1e243da10a6ed225847f7
Karan Bhamra <karan.bhamra@gmail.com>
2019-12-19 00:30:35 -0800

    removed unnecessary code in square draw function

--- ConsoleDrawing/ConsoleDrawing/Square.cpp
+++ ConsoleDrawing/ConsoleDrawing/Square.cpp
@@ -21,22 +21,7 @@ Square::Square(int xSp, int ySp, int xpos, int ypos, int width, int height, Util
 
 void Square::Draw(HDC dc, Util::Rectangle hitbox, Util::Color color)
 {
-	// erase previous hitbox
-	// we must use the RGB macro which has 3 parameters, each byte represents r,g, b
-
-	/*auto tempColor = Util::Color::Color(12, 12, 12);
-	for (size_t row = 0; row < height; row++)
-	{
-		for (size_t col = 0; col < width; col++)
-		{
-			SetPixel(dc, static_cast<int>(previousHitbox.getX() + col), static_cast<int>(previousHitbox.getY() + row), tempColor.getRGB());
-
-		}
-	}*/
-
-	//system("cls");
-
-	// draw current hitbox
+	// draw given hitbox with the given color, will switch between old hitbox and erasecolor and new hitbox and drawcolor
 	// we must use the RGB macro which has 3 parameters, each byte represents r,g, b
 	for (size_t row = 0; row < height; row++)
 	{
@@ -137,22 +122,4 @@ void Square::Intersects(Square& sq)
 		sq.SetXSpeed(tempx);
 		sq.SetYSpeed(tempy);
 	}
-	/*if (sq->x > x&& sq->y > y&& sq->x < x + width && sq->y < y + height)
-	{
-		int tmpX = xSpeed;
-		int tmpY = ySpeed;
-		xSpeed = sq->xSpeed;
-		ySpeed = sq->ySpeed;
-		sq->xSpeed = tmpX;
-		sq->ySpeed = tmpY;
-	}
-	else if (sq->width + sq->x > x&& sq->height + sq->y > y&& sq->x + sq->width < x + width && sq->y + sq->height < y + height)
-	{
-		int tmpX = xSpeed;
-		int tmpY = ySpeed;
-		xSpeed = sq->xSpeed;
-		ySpeed = sq->ySpeed;
-		sq->xSpeed = tmpX;
-		sq->ySpeed = tmpY;
-	}*/
 }
